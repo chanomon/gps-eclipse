60b927b5cd38761c5857606cecbd10ee361c28d1
ehuipe <hdomeli@gmail.com>
2024-02-01 15:45:24 -0600

    added more data

--- gps.py
+++ gps.py
@@ -28,8 +28,25 @@ try:
     for new_data in gps_socket:
         if new_data:
             data_stream.unpack(new_data)
-            print('Latitud = {}, Longitud = {}, Altitud = {}'.format(
-                data_stream.TPV['lat'], data_stream.TPV['lon'], data_stream.TPV['alt']))
+            #print('Latitud = {}, Longitud = {}, Altitud = {}'.format(
+            #    data_stream.TPV['lat'], data_stream.TPV['lon'], data_stream.TPV['alt']))
+            # Cheking if 2D fix or 3D fix
+            if data_stream.TPV['mode'] >= 2:
+                print('Latitud: {}'.format(data_stream.TPV['lat']))
+                print('Longitud: {}'.format(data_stream.TPV['lon']))
+                print('Altitud: {}'.format(data_stream.TPV['alt']))
+                print('Velocidad: {} m/s'.format(data_stream.TPV['speed']))
+                print('Rumbo: {} grados'.format(data_stream.TPV['track']))
+                print('Fecha: {}'.format(data_stream.TPV['time']))
+                print('Número de satélites visibles: {}'.format(data_stream.TPV['satellites_visible']))
+                print('Número de satélites utilizados: {}'.format(data_stream.TPV['satellites_used']))
+                print('Precisión horizontal: {} m'.format(data_stream.TPV['eph']))
+                print('Precisión vertical: {} m'.format(data_stream.TPV['epv']))
+                print('Modo de solución de posición: {}'.format(data_stream.TPV['mode']))
+                
+            else:
+                print('Esperando una fijación 2D o 3D...')
+
             
 except KeyboardInterrupt:
     print('\nCerrando la conexión con gpsd')
