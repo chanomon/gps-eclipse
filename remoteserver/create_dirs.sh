ccee40b0b35adad5c4214fbc0f56462c69afcf66
ehuipe <hdomeli@gmail.com>
2024-06-03 13:17:47 -0600

    input initial date, final date

--- remoteserver/create_dirs.sh
+++ remoteserver/create_dirs.sh
@@ -1,42 +1,30 @@
 #!/bin/bash
 
-# Run this script only once into the destination folder 
-# (more times only gives some warnings)
-mkdir 2024-03-06
-mkdir 2024-03-07
-mkdir 2024-03-08
-mkdir 2024-03-09
-mkdir 2024-03-10
-mkdir 2024-03-11
-mkdir 2024-03-12
-mkdir 2024-03-13
-mkdir 2024-03-14
-mkdir 2024-03-15
-mkdir 2024-03-16
-mkdir 2024-03-17
-mkdir 2024-03-18
-mkdir 2024-03-19
-mkdir 2024-03-20
-mkdir 2024-03-21
-mkdir 2024-03-22
-mkdir 2024-03-23
-mkdir 2024-03-24
-mkdir 2024-03-25
-mkdir 2024-03-26
-mkdir 2024-03-27
-mkdir 2024-03-28
-mkdir 2024-03-29
-mkdir 2024-03-30
-mkdir 2024-03-31
-mkdir 2024-04-01
-mkdir 2024-04-02
-mkdir 2024-04-03
-mkdir 2024-04-04
-mkdir 2024-04-05
-mkdir 2024-04-06
-mkdir 2024-04-07
-mkdir 2024-04-08
-mkdir 2024-04-09
-mkdir 2024-04-10
-mkdir 2024-04-11
-mkdir 2024-04-12
+# Verifica que se hayan proporcionado las fechas de inicio y fin
+if [ "$#" -ne 2 ]; then
+    echo "Uso: $0 fecha_inicio fecha_fin"
+    echo "Formato de fecha: YYYY-MM-DD"
+    exit 1
+fi
+
+
+fecha_inicio=$1
+fecha_fin=$2
+
+
+fecha_inicio_seg=$(date -d "$fecha_inicio" +%s)
+fecha_fin_seg=$(date -d "$fecha_fin" +%s)
+
+
+if [ "$fecha_inicio_seg" -gt "$fecha_fin_seg" ]; then
+    echo "La fecha de inicio debe ser anterior o igual a la fecha de fin"
+    exit 1
+fi
+
+
+fecha_actual_seg=$fecha_inicio_seg
+while [ "$fecha_actual_seg" -le "$fecha_fin_seg" ]; do
+    fecha_actual=$(date -d "@$fecha_actual_seg" +%Y-%m-%d)
+    mkdir -p "$fecha_actual"
+    fecha_actual_seg=$(($fecha_actual_seg + 86400))
+done
